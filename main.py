ef96af394a2420e7e4b9d8a33922c496e5e1d712
dasprince <dasprince06@gmail.com>
2021-01-31 01:46:53 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ while len(guessed_states) < 28:
 
     answer_state = screen.textinput(title=f"{len(guessed_states)}/28 states correct"
                                     , prompt="whats the state name").title()
-    if answer_state in state_name_list:
+    if answer_state in state_name_list and answer_state not in guessed_states:
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
@@ -29,6 +29,8 @@ while len(guessed_states) < 28:
         learn = pandas.DataFrame(learn)
         learn.to_csv("states_to_learn.csv")
         break
+    else:
+        pass
 
 
 
